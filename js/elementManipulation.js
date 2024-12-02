d2902a6632cf8c73243af288a089d03799494f72
Shadrack Essien <shadrack6960@gmail.com>
2024-12-02 23:30:51 +0000

    tailwindcss

--- js/elementManipulation.js
+++ js/elementManipulation.js
@@ -3,47 +3,50 @@ import { ProductService, Product } from "./product.js";
 export const createCard = (product) => {
   // card container
   const card = document.createElement("div");
-  card.classList.add("card");
+  card.className =
+    "my-4 border border-1-[#E0E0E0] rounded-lg bg-white flex flex-col gap-2 w-[421px] h-[488px] shadow-lg hover:shadow-lg transition-all duration-300 ease-in-out hover:scale-[1.02] text-black no-underline";
 
   // image
   const image = document.createElement("img");
-  image.classList.add("image");
+  image.className =
+    "w-auto h-1/2 object-cover object-center mx-auto mt-4 rounded-t-lg";
   image.src = product.image;
   image.alt = product.title;
   card.appendChild(image);
 
   // title
   const title = document.createElement("div");
-  title.classList.add("title");
+  title.className = "mx-4 text-[16px] font-semibold leading-[18.75px] mt-1";
   title.textContent = product.title;
   card.appendChild(title);
 
   // price
   const price = document.createElement("div");
-  price.classList.add("price");
+  price.className = "mx-4 text-[14px] font-medium leading-[16.41px] mb-2";
   price.textContent = `$ ${product.price.toFixed(2)}`;
   card.appendChild(price);
 
   // description
   const description = document.createElement("div");
-  description.classList.add("description");
+  description.className = "mx-4 mb-3 text-[14px] font-normal leading-[16.41px]";
   description.textContent = product.description;
   card.appendChild(description);
 
   // details
   const details = document.createElement("div");
-  details.classList.add("details");
+  details.className = "mx-4 flex flex-row items-center gap-1 mb-3";
 
   const link = document.createElement("a");
   link.href = `details.html?id=${product.id}`;
   link.textContent = "View details";
+  link.className = "text-primary text-[14px] leading-[16.41px] font-semibold";
 
   // SVG icon
   const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
   svg.setAttribute("viewBox", "0 0 16 16");
   svg.setAttribute("fill", "currentColor");
-  svg.classList.add("size-4");
+  svg.classList.add("text-primary", "size-4");
 
   const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
   path.setAttribute("fill-rule", "evenodd");
@@ -54,8 +57,8 @@ export const createCard = (product) => {
   path.setAttribute("clip-rule", "evenodd");
   svg.appendChild(path);
 
-  link.appendChild(svg);
   details.appendChild(link);
+  details.appendChild(svg);
   card.appendChild(details);
 
   return card;
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}", "./js/*js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        gilroy: ["Gilroy", "sans-serif"],
+      },
+      colors: {
+        primary: "#432361",
+      },
+      container: {
+        center: true,
+        padding: {
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
