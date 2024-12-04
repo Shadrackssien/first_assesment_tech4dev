55d4ebb2d883089ee1b1e6bcc19006b86a8b1316
Shadrack Essien <shadrack6960@gmail.com>
2024-12-04 03:04:20 +0000

    updated the product details page

--- js/details.js
+++ js/details.js
@@ -23,25 +23,87 @@ const displayProductDetails = (product) => {
   const detailsContainer = document.getElementById("product-details");
 
   detailsContainer.innerHTML = `
-  <div class="head">
-
-    <img class="image" src="${product.image}" alt="${product.title}" />
+  <!-- image -->
+  <div class="mx-16 my-8">
+    <img class="w-[100%] h-[480px] object-cover object-center" src="${
+      product.image
+    }" alt="${product.title}" />
   </div>
-  <div class="body">
-  <h1 class="title">${product.title}</h1>
-    <p class="price"><strong>Price:</strong> $ ${product.price.toFixed(2)}</p>
-    <p class="description"><strong>Description:</strong> ${
+  
+  <!-- Main content -->
+  <div class="flex gap-4">
+  <!-- Product content -->
+  <div>
+    <!-- title -->
+  <div class="mx-16 flex flex-col gap-2">
+  <h1 class="text-[24px] leading-[28.13px] font-bold w-[650px] h-[30px]">${
+    product.title
+  }</h1>
+
+  <!-- details -->
+  <div class="flex flex-col gap-[8px]">
+    <div class="flex flex-row gap-2 ">
+    <img class="w-[18px] h-[19.5px] mt-8" src="assets/vector1.png" alt="vector 1" />
+    <p class="mt-8">Price: $ ${product.price.toFixed(2)}</p>
+    </div>
+    <div class="flex flex-row gap-2 ">
+    <img class="w-[18px] h-[19.5px]" src="assets/vector2.png" alt="vector 1" />
+    <p>Category: ${product.category}</p>
+    </div>
+    <div class="flex flex-row gap-2 ">
+    <img class="w-[18px] h-[19.5px]" src="assets/vector3.png" alt="vector 1" />
+    <p>Rating: ${product.rating.rate} (${product.rating.count} reviews)</p>
+    </div>
+  </div>
+
+  <!-- description -->
+  <div class="mt-8 ">
+    <p class="font-bold text-[16px] leading-[18.75px] mb-4">Event description</p>
+    <p class="w-[875px]  text-[16px] leading-[18.75px]"> ${
       product.description
     }</p>
-    <p class="category"><strong>Category:</strong> ${product.category}</p>
-    <p class="rating"><strong>Rating:</strong> ${product.rating.rate} (${
-    product.rating.count
-  } reviews)</p>
-    <div class="buttons">
-    <button class="add-to-cart">Add to Cart</button>
-    <button class="buy-now">Buy Now</button>
+  </div>
+
+  <!-- Ticket Pricing -->
+  <div class="mt-16 ">
+    <p class="font-bold text-[16px] leading-[18.75px] mb-8">Tickets Pricing</p>
+    <div class="flex flex-row gap-16">
+    <div class="flex flex-col gap-2">
+    <p class="text-[20px] font-medium leading-[23.44px]" >Single</p>
+    <p class="text-[#9B51E0]">NGN 5,000</p>
     </div>
-    
+    <div class="flex flex-col gap-2">
+    <p class="text-[20px] font-medium leading-[23.44px]">Pair</p>
+    <p class="text-[#9B51E0]" >NGN 9,000</p>
+    </div>
+    </div>
+    <button class="button mt-16">Buy now</button>
+    </div>
+    </div>
+  </div>
+
+  <!-- Contact Orgainizers and Directions -->
+  <div>
+  <!-- Contact Organizers and Directions -->
+      <div>
+        <!-- Contact Organizers -->
+        <div class="flex flex-col gap-4 mt-16">
+        <p class="font-bold text-[16px] leading-[18.75px] mb-2">Contact Organizers</p>
+        <div class="flex flex-row gap-[24px]">
+        <img src="assets/p1.png" alt="p 1" />
+          <img src="assets/p2.png" alt="p 1" />
+          <img src="assets/p3.png" alt="p 1" />
+        </div>
+          
+        </div>
+
+        <!-- Directions -->
+        <div class="flex flex-col gap-4 mt-16">
+        <p class="font-bold text-[16px] leading-[18.75px] mb-2">Directions</p>
+        <img class="w-[400px] h-[452px]" src="assets/directions.png" alt="directions" />
+        </div>
+      </div>
+  </div>
   </div>
   `;
 };
